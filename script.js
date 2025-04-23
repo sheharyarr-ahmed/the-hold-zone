e0337569aa48c31c1aebfcdfa40563cb5cf0e199
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-04-23 12:20:58 +0500

    work has been started

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+//specifying variables
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+
+//intialise values
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
